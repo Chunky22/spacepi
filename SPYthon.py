95de11e87fa1d43dfed87cc1b974c5166ca5099b
= <san_calachaqui32@hotmail.com>
2019-07-11 22:45:04 -0600

    move photos to local folder

--- SPYthon.py
+++ SPYthon.py
@@ -4,6 +4,7 @@
 from picamera import PiCamera
 from time import sleep
 from datetime import datetime
+import os, sys, shutil
 
 #set up variables
 cur_dir = os.getcwd()
@@ -21,17 +22,17 @@ print("taking a picture spy.jpg")
 camera.capture(image_name)
 
 #sending file to Pi3b
-import os,sys
-sftp_command = """sftp slice@192.168.0.22:/home/slice/compute/SPYthon << EOF
-put %s
-chmod 777 /home/slice/compute/SPYthon/spy%s.jpg
-exit
-EOF""" % (image_name,cur_hour)
-print("transfering picture spy.jpg to rbpi3b")
 
-os.system(sftp_command)
+##sftp_command = """sftp slice@192.168.0.22:/home/slice/compute/SPYthon << EOF
+##put %s
+##chmod 777 /home/slice/compute/SPYthon/spy%s.jpg
+##exit
+##EOF""" % (image_name,cur_hour)
+##print("transfering picture spy.jpg to rbpi3b")
+#os.system(sftp_command)
 
-
+# move photos to local folder
+shutil.move(image_name, "~/Pictures/spy%s.jpg" % cur_hour)
 
 print("file transfer completed")
 os.system("rm %s" % image_name)
