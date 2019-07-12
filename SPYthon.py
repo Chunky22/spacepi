3745b93cb3e87710c89f808db52da36f9e6f10ae
= <san_calachaqui32@hotmail.com>
2019-07-11 21:48:09 -0600

    added python file to take still frames over time

---
+++ SPYthon.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+##https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/
+from picamera import PiCamera
+from time import sleep
+from datetime import datetime
+
+cur_date = datetime.now()
+cur_hour = cur_date.strftime('%H:%M')
+image_name = '/home/slice/Pictures/spy%s.jpg' % cur_hour
+camera = PiCamera()
+## set up settings
+camera.resolution = (2592,1944)
+camera.annotate_text = "timestamp: %s" %cur_date
+camera.annotate_text_size = 24
+
+##taking the picture
+print("taking a picture spy.jpg")
+camera.capture(image_name)
+
+#sending file to Pi3b
+import os,sys
+sftp_command = """sftp slice@192.168.0.22:/home/slice/compute/SPYthon << EOF
+put %s
+chmod 777 /home/slice/compute/SPYthon/spy%s.jpg
+exit
+EOF""" % (image_name,cur_hour)
+print("transfering picture spy.jpg to rbpi3b")
+
+os.system(sftp_command)
+
+
+
+print("file transfer completed")
+os.system("rm %s" % image_name)
+
+
